b55487bf6587762f864b0f6c6e1041f5101799a8
“heman-7” <makhijahemant007@gmail.com>
2018-05-30 02:17:54 +0530

    Extra Libraries Removed

--- app/src/main/java/com/example/heman/livescore/MainActivity.java
+++ app/src/main/java/com/example/heman/livescore/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.heman.livescore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,8 +9,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
